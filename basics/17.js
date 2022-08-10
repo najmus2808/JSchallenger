8e84445827f55e8ea92992ce61a149a9094d5aa9
najmus2808 <najmus2808@gmail.com>
2022-08-10 17:00:41 +0600

    Round a number to 2 decimal places

---
+++ basics/17.js
@@ -0,0 +1,13 @@
+// Write a function that takes a number (a) as argument
+// Round a to the 2nd digit after the comma
+// Return the rounded number
+
+function myFunction(a) {
+  return Number(a.toFixed(2)); // toFixed() make it string
+}
+
+// Test Cases:
+// myFunction(2.12397)  - Expected  : 2.12
+// myFunction(3.136)    - Expected  : 3.14
+// myFunction(1.12397)  - Expected  : 1.12
+// myFunction(26.1379)  - Expected  : 26.14
