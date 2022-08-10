9b3eae35ab316947acad367bdea941eedb44a092
najmus2808 <najmus2808@gmail.com>
2022-08-10 11:07:27 +0600

    Get type of value

--- basics/3.js
+++ basics/3.js
@@ -4,3 +4,11 @@
 function myFunction(a) {
   return typeof a;
 }
+
+// Test Cases:
+// myFunction(1)          -  Expected : 'number'
+// myFunction(false)      -  Expected : 'boolean'
+// myFunction({})         -  Expected : 'object'
+// myFunction(null)       -  Expected : 'object'
+// myFunction('string')   -  Expected : 'string'
+// myFunction(['array'])  -  Expected : 'object'
