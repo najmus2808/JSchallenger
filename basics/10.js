6eca1ccea1d5deb421f0344481545c6a3e14baf4
najmus2808 <najmus2808@gmail.com>
2022-08-10 10:48:48 +0600

    Return the percentage of a number

---
+++ basics/10.js
@@ -0,0 +1,12 @@
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
+
+// My Solution:
+function myFunction(a, b) {
+  return (a * b) / 100;
+}
+
+// Author Solution:
+function myFunction(a, b) {
+  return (b / 100) * a;
+}
